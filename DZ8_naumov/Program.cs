c516ef290e6512dc0fb4fc900d7a828e6e723176
inofuturei <vip.artemik@mail.ru>
2024-12-20 05:11:01 +0300

    Добавьте файлы проекта.

---
+++ DZ8_naumov/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var inputFilePath = "input.txt";
+        var outputFilePath = "output.txt";
+
+        try
+        {
+            var uniqueLines = ProcessFile(inputFilePath);
+            SaveToFile(uniqueLines, outputFilePath);
+            Console.WriteLine("Уникальные строки сохранены в output.txt");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"Файл {inputFilePath} не найден.");
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine($"Нет прав доступа к файлу {outputFilePath}.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+
+    static IEnumerable<string> ProcessFile(string filePath)
+    {
+        return File.ReadLines(filePath)
+                   .Distinct()
+                   .OrderBy(line => line);
+    }
+
+    static void SaveToFile(IEnumerable<string> lines, string filePath)
+    {
+        File.WriteAllLines(filePath, lines);
+    }
+}
